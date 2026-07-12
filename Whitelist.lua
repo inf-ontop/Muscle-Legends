2778e9181c293661e4d19b465e5d2fda179051b3
RuRu <inf69924@gmail.com>
2026-07-12 03:50:20 -0700

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -1,11 +1,12 @@
 return {
   ["thelegends23k"] = true,
   ["Yvngchase11"] = true,
+  ["o0RuuuuuRuuuuu0o"] = true,
   ["PFDxKISS2"] = true,
   ["PFDxkiss"] = true,
-  ["o0RuuuuuRuuuuu0o"] = true,
   ["HushedWit"] = true,
   ["rackedupkel2"] = true,
   ["idksumrandomig"] = true,
   ["dereine2010"] = true,
+  ["Xx_GPWArka"] = true,
 }
